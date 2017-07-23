dfb4ec05be0a70d7e94cff00960c3a657f6333ae
iTim <itim@outlook.com>
2017-07-24 00:00:02 +0700

    Show Code in ScheduleList

--- PCalendar/PCalendar/Models/ScheduleItem.cs
+++ PCalendar/PCalendar/Models/ScheduleItem.cs
@@ -1,6 +1,7 @@
 ﻿using SQLite;
 using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace PCalendar.Models
@@ -8,7 +9,7 @@ namespace PCalendar.Models
     public class ScheduleItem : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
             if (PropertyChanged != null)
             {
@@ -19,8 +20,6 @@ namespace PCalendar.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public DateTime ScheduleDate { get; set; }
-        public string Code1 { get; set; }
-        public string Code2 { get; set; }
         public bool IsPharmacy { get; set; }
         public TimeSpan PharmacyFrom { get; set; }
         public TimeSpan PharmacyTo { get; set; }
@@ -56,6 +55,48 @@ namespace PCalendar.Models
                     this._pharmacyTime = value;
                     NotifyPropertyChanged();
                 }
+            }
+        }
+
+        private string _code1;
+        public string Code1
+        {
+            get { return _code1; }
+            set
+            {
+                if (value != this._code1)
+                {
+                    _code1 = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged("CodeText");
+                }
+            }
+        }
+
+        private string _code2;
+        public string Code2
+        {
+            get { return _code2; }
+            set
+            {
+                if (value != this._code2)
+                {
+                    _code2 = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged("CodeText");
+                }
+            }
+        }
+
+        public string CodeText
+        {
+            get
+            {
+                string[] codes = new string[] { _code1, _code2 };
+                var workingCode = codes.Where(x => x.ToLower() != "x");
+                return workingCode.Count() > 0 ?
+                    string.Join(string.Empty, workingCode) :
+                    "X";
             }
         }
     }
